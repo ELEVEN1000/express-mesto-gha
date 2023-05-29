a6d3bf79d037cb4b8336e3492f36202e7bdcf84e
ELEVEN1000 <mikryuov.kostya@yandex.ru>
2023-05-29 20:37:12 +0700

    Небольшой рефакторинг кода

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
+
 const app = express();
-const { mongoose } = require('mongoose');
-const router = require('./routes');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const usersRouter = require('./users');
-const cardsRouter = require('./cards');
+const userRouter = require('./users');
+const cardRouter = require('./cards');
+const { ERROR_CODE_NOT_FOUND } = require('../constants');
 
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
+router.use('/users', userRouter);
+router.use('/cards', cardRouter);
+router.use('*', (req, res) => {
+  res.status(ERROR_CODE_NOT_FOUND).send({ message: `Страницы по адресу ${req.baseUrl} не существует` });
+});
 
 module.exports = router;
