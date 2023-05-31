7ad1671fea29304800b01848422483a022ef1a2d
ELEVEN1000 <mikryuov.kostya@yandex.ru>
2023-05-31 12:02:01 +0700

    13 пр. ревью 1 Рефакторинг кода

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
-const { ERROR_CODE_NOT_FOUND } = require('../utils/constants');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 router.use('*', (req, res) => {
-  res.status(ERROR_CODE_NOT_FOUND).send({ message: `Страницы по адресу ${req.baseUrl} не существует` });
+  res.status(404).send({ message: `Страницы по адресу ${req.baseUrl} не существует` });
 });
 
 module.exports = router;
