1493cb2699ce908593fda887e79f3ce18c0dce14
ELEVEN1000 <mikryuov.kostya@yandex.ru>
2023-06-10 15:30:37 +0700

    Первый коммит 14 пр_исправление ошибок

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { errors } = require('celebrate');
 const helmet = require('helmet');
+const { errors } = require('celebrate');
 const router = require('./routes/index');
 const { login, createUser } = require('./controllers/users');
 const { loginValidator, signupValidator } = require('./middlewares/validation');
 
-const { PORT = 3000 } = process.env;
 const errorHandler = require('./middlewares/errorHandler');
 const auth = require('./middlewares/auth');
-
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
@@ -18,15 +17,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 router.post('/signin', loginValidator, login);
 router.post('/signup', signupValidator, createUser);
 
-app.use(errorHandler);
 app.use(helmet());
-app.use(auth);
 app.use(router);
 app.use(errors());
+app.use(errorHandler);
+app.use(auth);
 
-app.listen(PORT, () => {
-});
+app.listen(PORT);
