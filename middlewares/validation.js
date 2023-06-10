1d8a8e1069236d39b30af9d5a31dd5dc20c51c48
ELEVEN1000 <mikryuov.kostya@yandex.ru>
2023-06-10 19:31:34 +0700

    Первый коммит 14 пр_исправление ошибок

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -2,14 +2,14 @@ const { celebrate, Joi } = require('celebrate');
 
 const regexLink = /^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\/[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?$/;
 
-const signinValidate = celebrate({
+const loginValidator = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
   }),
 });
 
-const signupValidate = celebrate({
+const signupValidator = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
@@ -19,44 +19,44 @@ const signupValidate = celebrate({
   }),
 });
 
-const userIdValidate = celebrate({
+const getUserByIdValidator = celebrate({
   params: Joi.object().keys({
     userId: Joi.string().hex().length(24),
   }),
 });
 
-const userInfoValidate = celebrate({
+const updateProfileValidator = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 });
 
-const userAvatarValidate = celebrate({
+const updateAvatarValidator = celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().regex(regexLink),
   }),
 });
 
-const cardValidate = celebrate({
+const createCardValidator = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().regex(regexLink),
   }),
 });
 
-const cardIdValidate = celebrate({
+const inputIdCardValidator = celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().hex().length(24),
   }),
 });
 
 module.exports = {
-  signinValidate,
-  signupValidate,
-  userIdValidate,
-  userInfoValidate,
-  userAvatarValidate,
-  cardValidate,
-  cardIdValidate,
+  loginValidator,
+  signupValidator,
+  getUserByIdValidator,
+  updateProfileValidator,
+  updateAvatarValidator,
+  createCardValidator,
+  inputIdCardValidator,
 };
