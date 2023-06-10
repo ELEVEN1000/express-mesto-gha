463de19447f9e8afece4f7d35689b448f4a4a106
ELEVEN1000 <mikryuov.kostya@yandex.ru>
2023-06-10 14:45:35 +0700

    Первый коммит 14 пр_исправление ошибок

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
+const path = require('path');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const router = require('./routes');
 const auth = require('./middlewares/auth');
+const errors = require('./middlewares/errors');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(cookieParser());
+app.use(require('./routes/index'));
 app.use(helmet());
-
+app.use(errors);
 app.use(auth);
 app.use(router);
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.listen(PORT);
+app.listen(PORT, () => {
+});
