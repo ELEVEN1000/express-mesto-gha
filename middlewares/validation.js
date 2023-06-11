31319aa0cd2822a5010c325cbe9b32320e6bb679
ELEVEN1000 <mikryuov.kostya@yandex.ru>
2023-06-11 16:50:51 +0700

    исправление автотестов 8

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -13,7 +13,7 @@ const signupValidator = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
-    name: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().regex(regexLink),
   }),
